1b71114c4ce47aaed6265d232a84262a19b4d732
Alexander Potapov <potapov.av.85@gmail.com>
2017-03-17 19:34:58 +0300

    Added render original page by short url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Url = require('../models/url');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'URL Shortener' });
+});
+
+/*GET */
+router.get('/:shortUrl', function (req, res) {
+  Url.findByShortUrl(req.params.shortUrl, function (err, result) {
+      if (err) {
+          res.render('index', { title: 'URL Shortener' });
+      }
+      res.redirect(301, result.originUrl);
+  });
 });
 
 module.exports = router;
